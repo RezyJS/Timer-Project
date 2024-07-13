3a9b5f41878140d39bdd0bb634f666d78f667ec5
Rezy <64067988+RezyIGL@users.noreply.github.com>
2024-07-13 19:18:55 +0300

    da

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ const timerDiv = document.querySelector(".timer"),
       stopTimerButton = timerDiv.querySelector("#stop"),
       resetTimerButton = timerDiv.querySelector("#reset");
 
+const h1 = document.querySelector("h1");
+
 let timerValue = 0;
 let timerInterval;
 
 function tick(textHolder, newTextValue) {
     textHolder.innerHTML = `${Math.floor(newTextValue / 60).toString().padStart(2, "0")}:${(newTextValue % 60).toString().padStart(2, "0")}`
 }
+
+h1.addEventListener("mouseenter", _ => {
+    document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+});
+
+h1.addEventListener("mouseleave", _ => {
+    document.body.style.backgroundColor = "white";
+})
 
 startTimerButton.addEventListener("click", (event) => {
     event.preventDefault();
